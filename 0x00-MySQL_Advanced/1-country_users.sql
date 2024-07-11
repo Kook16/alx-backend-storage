7969376a3fffd124ced556eb5f359df1d0a96965
kok <kok@ubuntu.ubuntu-domain>
2024-07-11 17:19:10 +0300

    fixed typo on task 1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -3,7 +3,7 @@
 -- If the table already exists, the script should not fail
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL,
